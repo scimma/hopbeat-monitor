0c4dbca77b1ae5234359f0e03b25777e6565f42d
Ron Tapia <rdt12@psu.edu>
2020-09-05 12:46:36 -0400

    Working in production, cleaning things up.

--- scripts/HeartUtils.py
+++ scripts/HeartUtils.py
@@ -1,18 +1,13 @@
 ### Python 3.6.8 does not have a nanosecond time function. I hate Python.
-### With Python, everyhing is alwasys "in some later version than the one
+### With Python, everything is alwasys "in some later version than the one
 ### that you are using". Well, except for threads.
 ###
 import subprocess
 import time
 import re
 
-###
-###
-###
-###
 def getStats (url):
     command = "/usr/local/bin/kafkacat -F /root/share/kafkacat.conf -b %s -C -t heartbeat -o -1 -e " % url
-    print("COMMAND: %s" % command)
 #    startTime = time.time_ns()
     startTime = int(time.time() * 1000000000)
 
@@ -43,4 +38,14 @@ def writeStats (url, db, ok, now, ktime, lb, cb, bl):
         command = "curl -sk -XPOST '%s/write?db=%s' -u $INFLUX_CREDS --data-binary \"hearbeat ok=%d,commtime=%f,beats=%d,beatlag=%d %d\"" % (url, db, ok, ktime, cb - lb, bl, now)
     else:
         command = "curl -sk -XPOST '%s/write?db=%s' -u $INFLUX_CREDS --data-binary \"hearbeat ok=%d\"" % (url, db, ok)
-    print("COMMAND: %s\n", command)
+    proc = subprocess.run([command], shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE, timeout=20)
+    iout = proc.stdout.decode().splitlines()
+    ierr = proc.stderr.decode().splitlines()
+    if (proc.returncode() != 0):
+        print("============================")
+        print("Error talking to influx.")
+        print("============================")
+        print(proc.stdout.decode().splitlines())
+        print("============================")
+        print(proc.stderr.decode().splitlines())
+        print("============================")

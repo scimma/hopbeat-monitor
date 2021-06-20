01c83e6e2556cf71bdd5d42b8ef4229c9a6e0c7e
Ron Tapia <rdt12@psu.edu>
2021-06-20 11:34:53 -0400

    add support for icinga check

--- scripts/Utils.py
+++ scripts/Utils.py
@@ -31,12 +31,6 @@ def getCredsString (region, secret):
   c = json.loads(cjson)
   return c["creds"]
 
-def getSecret (region, secret) {
-  cmd   = "/usr/local/bin/aws --region %s secretsmanager get-secret-value --secret-id %s" % (region, secret)
-  s = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE).stdout.read().decode()
-  return s
-}
-
 def writeConfig (loc, creds):
     cfh = open(loc, "w")
     cfh.write("security.protocol=SASL_SSL\n")
